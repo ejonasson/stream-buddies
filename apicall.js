e7dfac805ee2d5bb08f759be8e26430a3b1fb0e7
SpartanERK <spartanerk@spartanerk-desktop.(none)>
2014-03-23 14:57:25 -0400

    found out how to call the twitch API

--- apicall.js
+++ apicall.js
@@ -1,10 +1,25 @@
 // IMDb ID to Search
 $(document).ready(function() {
-	
-	var imdbId = "tt1285016";
-	var imdbUrl = "http://www.omdbapi.com/?i=" + imdbId;
-	$.getJSON(imdbUrl, function (data){
-			alert(data.Title);
-		})
-	
+console.log("1");
+Twitch.init({clientId: 'cbmag59uju3vb9fevpi2de3pank5wtg'}, function(error, status) {
+	console.log("Initialized");
+	callAPI();
+  });
 });
+
+function callAPI(){
+	var theURL = "https://api.twitch.tv/kraken/channels/spartanERK/follows";
+	console.log("starting function");
+$.ajax({
+            url: theURL,
+            type: 'GET',
+            contentType: 'application/json',
+            dataType: 'jsonp',
+            success: function(data) {
+             	console.log("In JSON function");
+				var twitchJSON = data;
+				console.log(twitchJSON);
+            }
+}); 
+}
+
