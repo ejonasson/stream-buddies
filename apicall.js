edce05f303db4ab19a0f87b72d0bb506d62aa2f5
SpartanERK <ejonasson@gmail.com>
2014-03-23 15:16:03 -0400

    modularized user find/url creation

--- apicall.js
+++ apicall.js
@@ -1,16 +1,8 @@
-// IMDb ID to Search
-$(document).ready(function() {
-console.log("1");
-Twitch.init({clientId: 'cbmag59uju3vb9fevpi2de3pank5wtg'}, function(error, status) {
-	console.log("Initialized");
-	callAPI();
-  });
-});
 
-function callAPI(){
-	var theURL = "https://api.twitch.tv/kraken/channels/spartanERK/follows";
+//Function to call the API based on other input data
+function callAPI(theURL){
 	console.log("starting function");
-$.ajax({
+	$.ajax({
             url: theURL,
             type: 'GET',
             contentType: 'application/json',
@@ -20,6 +12,24 @@ $.ajax({
 				var twitchJSON = data;
 				console.log(twitchJSON);
             }
-}); 
+		}); 
 }
+// A function to find a user. For now just hardcoded to me.
+function findUser(){
+	var username = "spartanERK";
+	return username;
+}
+
+$(document).ready(function() {
+console.log("1");
+
+var theUser = findUser();
+console.log(theUser);
+var theURL = "https://api.twitch.tv/kraken/users/" + theUser + "/follows/channels";
+console.log("2");
+Twitch.init({clientId: 'cbmag59uju3vb9fevpi2de3pank5wtg'}, function(error, status) {
+	console.log("Initialized");
+	callAPI(theURL);
+  });
+});
 
