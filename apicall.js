3b4d6c286babf8a65bfe11ad32dacfe67e39a874
ejonasson <ejonasson@gmail.com>
2014-03-24 17:01:36 -0400

    Working on logging in (still broken)

--- apicall.js
+++ apicall.js
@@ -1,12 +1,17 @@
+var theUser = findUser();
+var theURL = "https://api.twitch.tv/kraken/";
+var followsUrl = theURL + "users/" + theUser + "/follows/channels";
+var streamUrl = theURL + "streams/";
+
 // Function that finds the values requested in the API object
 function getValues(obj, key) {
     var objects = [];
     for (var i in obj) {
         if (!obj.hasOwnProperty(i)) continue;
-        if (typeof obj[i] == 'object') 
+        if (typeof obj[i] == 'object')
         {
             objects = objects.concat(getValues(obj[i], key));
-        } 
+        }
         else if (i == key) {
             objects.push(obj[key]);
         }
@@ -16,29 +21,46 @@ function getValues(obj, key) {
 
 // Add Stream Embeds to page
 function addStreams(streamNames){
-	for(var i = 0; i<streamNames.length; i++)
-	{
+		/*jshint multistr: true */
 		var embedCode = '<object type="application/x-shockwave-flash" height="378" width="620" id="live_embed_player_flash" \
-		data="http://www.twitch.tv/widgets/live_embed_player.swf?channel=' + streamNames[i] + '"bgcolor="#000000"> \
+		data="http://www.twitch.tv/widgets/live_embed_player.swf?channel=' + streamNames + '"bgcolor="#000000"> \
 		<param name="allowFullScreen" value="true" /><param name="allowScriptAccess" value="always" />  \
 		<param name="allowNetworking" value="all" /><param name="movie" value="http://www.twitch.tv/widgets/live_embed_player.swf" /> \
-		<param name="flashvars" value="hostname=www.twitch.tv&channel=' + streamNames[i] + '&auto_play=false&start_volume=25" /> \
-		</object><a href="http://www.twitch.tv/' + streamNames[i] + '" style="padding:2px 0px 4px; display:block \
+		<param name="flashvars" value="hostname=www.twitch.tv&channel=' + streamNames + '&auto_play=false&start_volume=25" /> \
+		</object><a href="http://www.twitch.tv/' + streamNames + '" style="padding:2px 0px 4px; display:block \
 		width:345px; font-weight:normal; font-size:10px;text-decoration:underline; text-align:center;"></a>';
 		$('#streamarea').append(embedCode);
+		
+}
+
+function showOnline(stream){
+	var userURL = streamUrl + stream;
+	console.log(userURL);
+	$.ajax({
+		url: userURL,
+		type: 'GET',
+		contentType: 'application/json',
+		dataType: 'jsonp',
+		cache: true,
+		success: function(data) {
+			if (data.stream !== null){
+			console.log(data);
+			addStreams(stream);
+			}
 		}
+	});
 }
 
 // The function that is called when JSON is found
 
 function useJSON(JSON){
 	console.log(JSON);
-// Find a good way to just have both of these in one function
-	var streamUrls = getValues(JSON, 'url');
 	var streamNames = getValues(JSON, 'name');
-	console.log(streamUrls);
 	console.log(streamNames);
-	addStreams(streamNames);
+	for (var i = 0; i<streamNames.length; i++){
+		console.log(streamNames[i]);
+		//showOnline(streamNames[i]);
+	}
 }
 
 // A function to find a user. For now just hardcoded to me.
@@ -48,24 +70,23 @@ function findUser(){
 }
 //Function to call the API based on other input data
 function followers(theURL){
-	$.ajax({
+		$.ajax({
             url: theURL,
             type: 'GET',
             contentType: 'application/json',
             dataType: 'jsonp',
+            cache: true,
             success: function(data) {
 				useJSON(data);
             }
-		}); 
+		});
 }
 
 $(document).ready(function() {
 
-var theUser = findUser();
-var theURL = "https://api.twitch.tv/kraken/users/" + theUser + "/follows/channels";
+console.log(streamUrl);
 Twitch.init({clientId: 'cbmag59uju3vb9fevpi2de3pank5wtg'}, function(error, status) {
-	followers(theURL);
-	// current issue: not taking JSON out of function
+	followers(followsUrl);
   });
 });
 
