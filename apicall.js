6ad33e093ab7dbe0da7437b143ae3a743a205f71
SpartanERK <ejonasson@gmail.com>
2014-03-24 23:04:29 -0400

    realized I was overcomplcating - cleaned up function order for readability

--- apicall.js
+++ apicall.js
@@ -6,8 +6,40 @@ var followsUrl = theURL + "users/" + theUser + "/follows/channels";
 var streamUrl = theURL + "streams/";
 var template = _.template($('#testtemplate').html()
 	);
-var potato = template();
+
+// A function to find a user. For now just hardcoded to me.
+function findUser(){
+	var username = "spartanerk";
+	return username;
+}
+
+//Function to call the API based on other input data
+function followers(theURL){
+		$.ajax({
+            url: theURL,
+            type: 'GET',
+            contentType: 'application/json',
+            dataType: 'jsonp',
+            cache: true,
+            success: function(data) {
+				useJSON(data);
+            }
+		});
+}
+
+function useJSON(JSON){
+
+	console.log(JSON);
+	var streamNames = getValues(JSON, 'name');
+	console.log(streamNames);
+	for (var i = 0; i<streamNames.length; i++){
+		showOnline(streamNames[i]);
+
+	}
+
+}
 // Function that finds the values requested in the API object
+
 function getValues(obj, key) {
     var objects = [];
     for (var i in obj) {
@@ -23,16 +55,6 @@ function getValues(obj, key) {
     return objects;
 }
 
-// Add Stream Embeds to page
-function addStreams(streamName){
-		console.log("enterstreams");
-	for (var i = 0; i<streamName.length; i++)
-		{
-			console.log(streamName[i]);
-			$('#streamarea').append(template());
-		}
-}
-
 function showOnline(stream){
 	var userURL = streamUrl + stream;
 	console.log(userURL);
@@ -44,48 +66,27 @@ function showOnline(stream){
 		cache: true,
 		success: function(data) {
 			if (data.stream !== null){
-				console.log("f");
-				validStream.push(stream);
-				console.log(validStream);
+				addStream(stream);
 			}
 		}
 	});
 }
 
+// Add Stream Embeds to page
+function addStream(streamName){
+		console.log("enterstreams");
+			console.log(streamName);
+			$('#streamarea').append(template());
+		
+}
+
+
+
 
 // The function that is called when JSON is found
 
-function useJSON(JSON){
 
-	console.log(JSON);
-	var streamNames = getValues(JSON, 'name');
-	console.log(streamNames);
-	for (var i = 0; i<streamNames.length; i++){
-		showOnline(streamNames[i]);
 
-	}
-	addStreams(validStream);
-
-}
-
-// A function to find a user. For now just hardcoded to me.
-function findUser(){
-	var username = "spartanerk";
-	return username;
-}
-//Function to call the API based on other input data
-function followers(theURL){
-		$.ajax({
-            url: theURL,
-            type: 'GET',
-            contentType: 'application/json',
-            dataType: 'jsonp',
-            cache: true,
-            success: function(data) {
-				useJSON(data);
-            }
-		});
-}
 
 $(document).ready(function() {
 
