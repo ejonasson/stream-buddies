5ce52331c8a75dbc3f4259e4517aef662f2df46e
ejonasson <ejonasson@gmail.com>
2014-03-26 19:41:19 -0400

    simplify the showing stream check

--- js/apicall.js
+++ js/apicall.js
@@ -3,7 +3,7 @@ var theUser = findUser();
 var theURL = "https://api.twitch.tv/kraken/";
 var followsUrl = theURL + "users/" + theUser + "/follows/channels";
 var streamUrl = theURL + "streams/";
-
+var showingStream = false;
 // A function to find a user. For now just hardcoded to me.
 function findUser(){
 	var username = "spartanerk";
@@ -12,16 +12,7 @@ function findUser(){
 
 //Function to call the API based on other input data
 function followers(theURL){
-		$.ajax({
-            url: theURL,
-            type: 'GET',
-            contentType: 'application/json',
-            dataType: 'jsonp',
-            cache: true,
-            success: function(data) {
-				useJSON(data);
-            }
-		});
+
 }
 
 function useJSON(JSON){
@@ -38,6 +29,7 @@ function useJSON(JSON){
 function showOnline(streamArray){
 	var name = streamArray.name;
 	var userURL = streamUrl + name;
+	//reset stream list for new import
 	$.ajax({
 		url: userURL,
 		type: 'GET',
@@ -60,29 +52,38 @@ function showOnline(streamArray){
 
 // Add Stream Embeds to page
 function addStream(followArray, streamObject){
-			var streamObj = {
-				channelName : followArray.name,
-				viewerCount : viewerCount(streamObject.viewers),
-				game		: followArray.game,
-				status		: followArray.status,
-				logo		: followArray.logo,
-				display_name: followArray.display_name,
-				};
-			var source = $('#stream-lister').html();
-			var template = Handlebars.compile(source);
-			$('#streamer-list').append(template(streamObj));
+	var streamObj = {
+		channelName : followArray.name,
+		viewerCount : viewerCount(streamObject.viewers),
+		game		: followArray.game,
+		status		: followArray.status,
+		logo		: followArray.logo,
+		display_name: followArray.display_name,
+	};
+	var source = $('#stream-lister').html();
+	var template = Handlebars.compile(source);
+	$('#streamer-list').append(template(streamObj));
 			// To do: make which stream is featured somewhat more random
-			var getFirstStreamID = $( "#streamer-list > :first-child").attr('id');
+	var getFirstStreamID = $( "#streamer-list > :first-child").attr('id');
 
-			//Check to see if this is the first stream loaded. If so, add the preview box
+		/*	//Check to see if this is the first stream loaded. If so, add the preview box
 			if (getFirstStreamID == streamObj.channelName){
-					changeStream(streamObj.channelName);
+				changeStream(streamObj.channelName);
 					// Turn stream ID into an actual ID
 					var FirstStreamID = "#" + getFirstStreamID;
 					$(FirstStreamID).addClass('selected-stream');
-			}
-		
+
+					*/
+	if(!showingStream)
+	{
+		showingStream = true;
+		changeStream(streamObj.channelName);
+	}
+	// Turn stream ID into an actual ID
+	var FirstStreamID = "#" + getFirstStreamID;
+	$(FirstStreamID).addClass('selected-stream');
 }
+
 
 
 function viewerCount(viewers){
@@ -113,12 +114,26 @@ function changeStream(streamer){
 
 
 $(document).ready(function() {
-
-	Twitch.init({clientId: 'cbmag59uju3vb9fevpi2de3pank5wtg'}, function(error, status) {
-	//todo: set timer to run this every 60 seconds. Need to either clear every time or append instead of erasing
-		followers(followsUrl);
-
+	function queryTwitch(){
+		Twitch.init({clientId: 'cbmag59uju3vb9fevpi2de3pank5wtg'}, function(error, status) {
+		//todo: separate the stream call from the button call
+		//So that the stream doesn't have to re-load
+		$.ajax({
+			url: followsUrl,
+			type: 'GET',
+			contentType: 'application/json',
+			dataType: 'jsonp',
+			cache: true,
+			success: function(data) {
+				useJSON(data);
+			}
+		});
+		console.log("ping");
 	});
+	}
+	queryTwitch();
+
+
 	//Currently only adds class, doesn't remove from exisitng
 	$(document).on('click', '.streamer', function(){
 		$('.selected-stream').removeClass('selected-stream');
