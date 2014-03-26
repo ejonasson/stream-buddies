397f6458a23fdeac971359766f6f1ee564ccce16
SpartanERK <ejonasson@gmail.com>
2014-03-25 20:29:39 -0400

    cleaned up API implementation, call/template has more variables

--- apicall.js
+++ apicall.js
@@ -27,32 +27,17 @@ function followers(theURL){
 function useJSON(JSON){
 
 	console.log(JSON);
-	var streamNames = getValues(JSON, 'name');
-	for (var i = 0; i<streamNames.length; i++){
-		showOnline(streamNames[i]);
+	var follows = JSON.follows;
+	for (var i = 0; i<follows.length; i++){
+		var streamArray = follows[i]['channel'];
+		showOnline(streamArray);
 
 	}
 
 }
-// Function that finds the values requested in the API object
-
-function getValues(obj, key) {
-    var objects = [];
-    for (var i in obj) {
-        if (!obj.hasOwnProperty(i)) continue;
-        if (typeof obj[i] == 'object')
-        {
-            objects = objects.concat(getValues(obj[i], key));
-        }
-        else if (i == key) {
-            objects.push(obj[key]);
-        }
-    }
-    return objects;
-}
-
-function showOnline(stream){
-	var userURL = streamUrl + stream;
+function showOnline(streamArray){
+	var name = streamArray['name'];
+	var userURL = streamUrl + name;
 	console.log(userURL);
 	$.ajax({
 		url: userURL,
@@ -63,18 +48,26 @@ function showOnline(stream){
 		success: function(data) {
 			console.log(data);
 			if (data.stream !== null){
-				addStream(stream, data);
+				addStream(streamArray, data.stream);
 			}
 		}
 	});
 }
 
 // Add Stream Embeds to page
-function addStream(streamName, object){
-		console.log("enterstreams");
-			console.log(streamName);
-			// Let's figure out how to get this object to contain everything we need from both API calls
-			var streamObj = {channelName : streamName};
+function addStream(followArray, streamObject){
+			console.log("followArray:"); 
+			console.log(followArray);
+			console.log("Streamobject ");
+			console.log(streamObject);
+			var streamObj = {
+				channelName : followArray['name'],
+				viewerCount : viewerCount(streamObject['viewers']),
+				game		: followArray['game'],
+				status		: followArray['status'],
+				logo		: streamObject['channel']['logo'],
+
+				}
 			var source = $('#streamembed').html();
 			var template = Handlebars.compile(source);
 			$('#streamarea').append(template(streamObj));
@@ -97,9 +90,26 @@ function viewerCount(viewers){
 
 $(document).ready(function() {
 
-console.log(streamUrl);
 Twitch.init({clientId: 'cbmag59uju3vb9fevpi2de3pank5wtg'}, function(error, status) {
+//todo: set timer to run this every 60 seconds. Need to either clear every time or append instead of erasing
 	followers(followsUrl);
   });
 });
 
+// Function that finds the values requested in the API object - no longer needed, left in for reference
+/*
+function getValues(obj, key) {
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == 'object')
+        {
+            objects = objects.concat(getValues(obj[i], key));
+        }
+        else if (i == key) {
+            objects.push(obj[key]);
+        }
+    }
+    return objects;
+}
+*/
