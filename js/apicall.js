20ab1275450836bf39dd9ca976267a3717c0b172
ejonasson <ejonasson@gmail.com>
2014-03-26 22:47:17 -0400

    fixed issue with duplicating stream items during successive API calls

--- js/apicall.js
+++ js/apicall.js
@@ -1,4 +1,5 @@
-var streamCount = 0;
+var loadedStreams = [];
+var offlineStreams = [];
 var theUser = findUser();
 var theURL = "https://api.twitch.tv/kraken/";
 var followsUrl = theURL + "users/" + theUser + "/follows/channels";
@@ -17,7 +18,6 @@ function followers(theURL){
 
 function useJSON(JSON){
 
-	console.log(JSON);
 	var follows = JSON.follows;
 	for (var i = 0; i<follows.length; i++){
 		var streamArray = follows[i].channel;
@@ -37,15 +37,23 @@ function showOnline(streamArray){
 		contentType: 'application/json',
 		dataType: 'jsonp',
 		cache: true,
+		async: false,
 		success: function(data) {
 			if (data.stream !== null){
-				console.log(data.stream);
 				streamViewers = data.stream['viewers'];
 				addStream(streamArray, streamViewers);
-				streamCount++;
 			}
 			else{
-				$('#offline-list').append(name + " ");
+				var inList = false;
+				for (var i in offlineStreams){
+					if (offlineStreams[i] === name){
+						inList = true;
+					}
+				}
+				if (!inList){
+					$('#offline-list').append(name + " ");
+					offlineStreams.push(name);
+				}
 			}
 		}
 	});
@@ -55,37 +63,57 @@ function showOnline(streamArray){
 
 // Add Stream Embeds to page
 function addStream(followArray, streamViewers){
-	var streamObj = {
-		channelName : followArray.name,
-		viewerCount : viewerCount(streamViewers),
-		game		: followArray.game,
-		status		: followArray.status,
-		logo		: followArray.logo,
-		display_name: followArray.display_name,
-	};
-	console.log(followArray);
-	var source = $('#stream-lister').html();
-	var template = Handlebars.compile(source);
-	$('#streamer-list').append(template(streamObj));
-			// To do: make which stream is featured somewhat more random
-	var getFirstStreamID = $( "#streamer-list > :first-child").attr('id');
+	function checkIfLoaded() {
+		for (var i = 0; i<loadedStreams.length; i++){
+			if (loadedStreams[i].channelName === followArray.name){
+				return true;
+			}
+			else{
+			}
 
-		/*	//Check to see if this is the first stream loaded. If so, add the preview box
-			if (getFirstStreamID == streamObj.channelName){
-				changeStream(streamObj.channelName);
-					// Turn stream ID into an actual ID
-					var FirstStreamID = "#" + getFirstStreamID;
-					$(FirstStreamID).addClass('selected-stream');
-
-					*/
-	if(!showingStream)
-	{
-		showingStream = true;
-		changeStream(streamObj.channelName);
+		}
+	return false;
 	}
-	// Turn stream ID into an actual ID
-	var FirstStreamID = "#" + getFirstStreamID;
-	$(FirstStreamID).addClass('selected-stream');
+	var streamObj = {
+		 
+			channelName : followArray.name,
+			viewerCount : viewerCount(streamViewers),
+			game		: followArray.game,
+			status		: followArray.status,
+			logo		: followArray.logo,
+			display_name: followArray.display_name,
+			already_loaded: checkIfLoaded(loadedStreams)
+		};
+	if (streamObj.already_loaded){
+		// Todo: update stream ifno?
+	}
+	else{
+		var source = $('#stream-lister').html();
+		var template = Handlebars.compile(source);
+		var thisStream = streamObj;
+		loadedStreams.push(streamObj)
+		console.log(loadedStreams);
+		$('#streamer-list').append(template(thisStream));
+
+				// To do: make which stream is featured somewhat more random
+		var getFirstStreamID = $( "#streamer-list > :first-child").attr('id');
+		if (getFirstStreamID == streamObj.channelName){
+			changeStream(streamObj.channelName);
+			// Turn stream ID into an actual ID
+			var FirstStreamID = "#" + getFirstStreamID;
+			$(FirstStreamID).addClass('selected-stream');
+		}				
+		if(!showingStream)
+		{
+			showingStream = true;
+			changeStream(streamObj.channelName);
+		}
+		// Turn stream ID into an actual ID
+		var FirstStreamID = "#" + getFirstStreamID;
+		$(FirstStreamID).addClass('selected-stream');
+
+	}
+	
 }
 
 
@@ -116,9 +144,9 @@ function changeStream(streamer){
 
 
 
-
 $(document).ready(function() {
 	function queryTwitch(){
+		console.log("Requesting data from Twitch");
 		Twitch.init({clientId: 'cbmag59uju3vb9fevpi2de3pank5wtg'}, function(error, status) {
 		//todo: separate the stream call from the button call
 		//So that the stream doesn't have to re-load
@@ -128,16 +156,18 @@ $(document).ready(function() {
 			contentType: 'application/json',
 			dataType: 'jsonp',
 			cache: true,
+			async: false,
 			success: function(data) {
+				console.log("success");
 				useJSON(data);
-			}
+			},
+
 		});
-		console.log("ping");
 	});
 	}
 	queryTwitch();
-
-
+	setInterval(queryTwitch, 60000);
+	
 	//Currently only adds class, doesn't remove from exisitng
 	$(document).on('click', '.streamer', function(){
 		$('.selected-stream').removeClass('selected-stream');
