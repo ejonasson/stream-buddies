c9092ada2738f27a3be311aa81c4661fa383f00b
ejonasson <ejonasson@gmail.com>
2014-03-26 23:08:26 -0400

    add query search for streams

--- js/apicall.js
+++ js/apicall.js
@@ -6,8 +6,24 @@ var followsUrl = theURL + "users/" + theUser + "/follows/channels";
 var streamUrl = theURL + "streams/";
 var showingStream = false;
 // A function to find a user. For now just hardcoded to me.
+
+// get Query Variables. Code courtesy of CSS Tricks
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){return pair[1];}
+       }
+       return(false);
+}
 function findUser(){
 	var username = "spartanerk";
+	var queryname = getQueryVariable("name");
+	if (queryname){
+		return queryname
+	}
 	return username;
 }
 
