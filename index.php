521d82246d86bbfd3c96bd66805ae5cbf6c2f5c4
SpartanERK <ejonasson@gmail.com>
2014-04-07 19:53:12 -0400

    Testing curl/php api call

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <?php
 //	define('__ROOT__', dirname(dirname(__FILE__)));
 
-	require_once 'templates/head.php';
-	require_once 'templates/streams.php';
-	require_once 'templates/footer.php';
+//	require_once 'templates/head.php';
+//	require_once 'templates/streams.php';
+//	require_once 'templates/footer.php';
+
+function get_url_contents($url){
+    $crl = curl_init();
+    $timeout = 5;
+    curl_setopt ($crl, CURLOPT_URL,$url);
+    curl_setopt ($crl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt ($crl, CURLOPT_CONNECTTIMEOUT, $timeout);
+    $ret = curl_exec($crl);
+    curl_close($crl);
+    return $ret;
+}
+
+
+  $streams = json_decode(get_url_contents("https://api.twitch.tv/kraken/channels/spartanERK/follows"));
+  var_dump($streams);
 
 ?>
