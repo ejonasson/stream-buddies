ec4da1667ff532b3463d79abf31fe86967690ee8
ejonasson <ejonasson@gmail.com>
2014-04-11 14:51:28 -0400

    set per-user recommendations limit to 10

--- js/recommendations.js
+++ js/recommendations.js
@@ -1,3 +1,4 @@
+var recommended = {};
 $(document).ready(function() {
 //get twitch follows
 queryTwitch(followsUrl, secondFollows, returned404);
@@ -9,10 +10,9 @@ queryTwitch(followsUrl, secondFollows, returned404);
 function secondFollows(streams){
 	var follows_order1 = {};
 	var follows = streams.follows;
-	console.log(follows);
 	for (var i in follows){
 		var name = follows[i].channel.name;
-		var streamUrl = theURL + "channels/" + name + "/follows/";
+		var streamUrl = theURL + "channels/" + name + "/follows?limit=10";
 		queryTwitch (streamUrl, loadFollowers, returned404);
 	}
 }
@@ -22,14 +22,44 @@ function loadFollowers(data){
 	var follower = data.follows;
 	for (var i in follower){
 		var thisFollower = follower[i].user.name;
-		console.log(thisFollower);
+		var followerURL = theURL + "users/" + thisFollower + "/follows/channels?limit=10";
+			queryTwitch (followerURL, recommendations, returned404);
 	}
 }
 
 // 3. Find and total up this sample's list of follows
+function recommendations(data){
+	var stream = [];
+	var follows = data.follows;
+	for (var i in follows){
+		var streamArray = follows[i].channel;
+		console.log(streamArray);
+		for (var j in recommended){
+			if (recommended[j] === streamArray){
+				recommended[j].count++;
+			}
+			else {
+				stream = {
+					channelName : streamArray.name,
+					game        : streamArray.game,
+					status      : streamArray.status,
+					logo        : streamArray.logo,
+					logoid      : streamArray.name + "-logo",
+					display_name : streamArray.display_name,
+					online       : false,
+					already_loaded : false,
+					metaID       : streamArray.name + "-meta",
+					count        : 1
+				};
+				recommended.push(streamArray);
+			}
+		}
+	}
+	console.log(recommended);
+}
 
 
-// 4. Find the top 3 overall recommendations
+// 4. Find the top 3 overall recommendations - check against loadedstreams (have to build that too)
 
 // 5. Also Find the top 3 (or however many exist) Currently online
 
